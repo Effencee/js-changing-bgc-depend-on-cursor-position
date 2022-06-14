0958ef5f984d1bcda0c33bcc7937c240ceeb3522
Effencee <107486971+Effencee@users.noreply.github.com>
2022-06-14 17:50:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
-
-// jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
-// jesli wartość dla x i y jest nieparzysta to kolor niebieski
-// jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
 document.body.addEventListener('click', e =>{
     console.log(e.clientX, e.clientY);
     let x = e.clientX;
